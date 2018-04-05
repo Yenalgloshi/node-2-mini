a5fcfd69ec5c5db24e42621e1b390e58694fc0f2
Yenalgloshi <jasonbegay@msn.com>
2018-04-05 15:42:15 -0600

    first push

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    dbconnection:'postgres://ygxdfomcdplxle:aea3370a627d0b2d7b436ddbc462edfb08421d18ccd493f01b6fbedd8da40f87@ec2-54-204-21-226.compute-1.amazonaws.com:5432/d7ejundcarspbt?ssl=true'
+}
--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+require('dotenv').config()
+const controller = require('./controller');
 
 const app = express();
+massive( process.env.CONNECTION_STRING ).then( dbInstance => {
+  app.set('db', dbInstance);
+  console.log('db connected')
+
+    // dbInstance.new_planes()
+    //   .then( planes => console.log( planes ) )
+    //   .catch( err => console.log( err ) );
+
+    // dbInstance.get_planes()
+    //     .then( planes => console.log( planes ) )
+    //     .catch( err => console.log( err ) );
+}).catch((err) => {
+    console.log('ERROR',err)
+})
+
 app.use( bodyParser.json() );
 app.use( cors() );
 
+app.get('/api/planes', controller.getPlanes);
+
 const port = process.env.PORT || 3000
-app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
+app.listen( port , () => { console.log(`Area 51 is listening on port ${port}`); } );
 
